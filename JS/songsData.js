cbb43a41bd68c8cb14a5dd9469631b16fa08c813
RahmaElbially <rahmamohammed117@gmail.com>
2023-08-01 00:47:20 +0300

    Music Player

---
+++ JS/songsData.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name : 'song 1',
+        path : 'Songs/Song 1.mp3',
+        artist : 'artist 1',
+        cover : 'Images/cover 1.png',
+    },
+    {
+        name : 'song 2',
+        path : 'Songs/Song 2.mp3',
+        artist : 'artist 2',
+        cover : 'Images/cover 2.png',
+    },
+    {
+        name : 'song 3',
+        path : 'Songs/Song 3.mp3',
+        artist : 'artist 3',
+        cover : 'Images/cover 3.png',
+    },
+    {
+        name : 'song 4',
+        path : 'Songs/Song 4.mp3',
+        artist : 'artist 4',
+        cover : 'Images/cover 4.png',
+    },
+    {
+        name : 'song 5',
+        path : 'Songs/Song 5.mp3',
+        artist : 'artist 5',
+        cover : 'Images/cover 5.png',
+    },
+];
